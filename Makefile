49ccb272351e53e85a8c2dd864ea3b0d5be152e2
John Nario <johnnario@Johns-MacBook-Air.local>
2025-04-25 01:54:23 -0400

    Added user variables to make file

--- Makefile
+++ Makefile
@@ -1,15 +1,20 @@
+CC = arm-none-eabi-gcc
+CFLAGS = -c -mcpu=cortex-m4 -mthumb -std=gnu11
+LDFLAGS = -nostdlib -T stm32_ls.ld -Wl,-Map=stm32_blink.map
+
 final : stm32_blink.elf
 
 main.o : main.c
-	arm-none-eabi-gcc -c -mcpu=cortex-m4 -mthumb -std=gnu11 main.c -o main.o
+	$(CC) $(CFLAGS) $^ -o $@
 	
 stm32f446_startup.o : stm32f446_startup.c
-	arm-none-eabi-gcc -c -mcpu=cortex-m4 -mthumb -std=gnu11 stm32f446_startup.c -o stm32f446_startup.o
+	$(CC) $(CFLAGS) $^ -o $@
 	
 stm32_blink.elf : main.o stm32f446_startup.o
-	arm-none-eabi-gcc -nostdlib -T stm32_ls.ld *.o -o stm32_blink.elf -Wl,-Map=stm32_blink.map
+	$(CC) $(LDFLAGS) $^ -o $@
 
 load :
 	openocd -f board/st_nucleo_f4.cfg
+
 clean:
-	del	-f *.o *.elf *.map
+	rm -f *.o *.elf *.map
--- stm32f446_startup.c
+++ stm32f446_startup.c
@@ -4,7 +4,7 @@
 
 /**Build:
 main.o :  arm-none-eabi-gcc -c -mcpu=cortex-m4 -mthumb -std=gnu11 main.c -o main.o
-stm32f411_startup.o : arm-none-eabi-gcc -c -mcpu=cortex-m4 -mthumb -std=gnu11 stm32f411_startup.c -o stm32f411_startup.o
+stm32f411_startup.o : arm-none-eabi-gcc -c -mcpu=cortex-m4 -mthumb -std=gnu11 stm32f446_startup.c -o stm32f446_startup.o
 stm32_blink.elf : arm-none-eabi-gcc -nostdlib -T stm32_ls.ld *.o -o stm32_blink.elf -Wl,-Map=stm32_blink.map
 
 Load:
@@ -143,113 +143,115 @@ void FMPI2C1_IRQHandler              	(void) __attribute__ ((weak, alias("Defaul
 void FMPI2C1_error_IRQHandler              	(void) __attribute__ ((weak, alias("Default_Handler"))); 
 
 /* Vector Table */
+__attribute__((section(".isr_vector_tbl")))
+uint32_t vector_tbl[] = {
+    /* Stack pointer */
+    (uint32_t)&_estack,
 
-uint32_t vector_tbl[] __attribute__((section(".isr_vector_tbl")))={
-	
-	(uint32_t)&_estack,
-	(uint32_t)&Reset_Handler,
-	(uint32_t)&NMI_Handler,
-	(uint32_t)&HardFault_Handler,
-	(uint32_t)&MemManage_Handler,
-	(uint32_t)&BusFault_Handler,
-  (uint32_t)&UsageFault_Handler,
-  0,
-  0,
-  0,
-  0,
-  (uint32_t)&SVC_Handler,
-  (uint32_t)&DebugMon_Handler,
-  0,
-  (uint32_t)&PendSV_Handler,
-  (uint32_t)&SysTick_Handler,
-  (uint32_t)&WWDG_IRQHandler,              			/* Window Watchdog interrupt                                          */
-  (uint32_t)&PVD_IRQHandler,               			/* EXTI Line 16 interrupt / PVD through EXTI                          */
-  (uint32_t)&TAMP_STAMP_IRQHandler,        			/* Tamper and TimeStamp interrupts through                            */
-  (uint32_t)&RTC_WKUP_IRQHandler,          			/* RTC Wakeup interrupt through the EXTI line                         */
-  (uint32_t)&FLASH_IRQHandler,             			/* FLASH global interrupt                                             */
-  (uint32_t)&RCC_IRQHandler,               			/* RCC global interrupt                                               */
-  (uint32_t)&EXTI0_IRQHandler,             			/* EXTI Line0 interrupt                                               */
-  (uint32_t)&EXTI1_IRQHandler,             			/* EXTI Line1 interrupt                                               */
-  (uint32_t)&EXTI2_IRQHandler,             			/* EXTI Line2 interrupt                                               */
-  (uint32_t)&EXTI3_IRQHandler,             			/* EXTI Line3 interrupt                                               */
-  (uint32_t)&EXTI4_IRQHandler,             			/* EXTI Line4 interrupt                                               */
-  (uint32_t)&DMA1_Stream0_IRQHandler,      			/* DMA1 Stream0 global interrupt                                      */
-  (uint32_t)&DMA1_Stream1_IRQHandler,      			/* DMA1 Stream1 global interrupt                                      */
-  (uint32_t)&DMA1_Stream2_IRQHandler,      			/* DMA1 Stream2 global interrupt                                      */
-  (uint32_t)&DMA1_Stream3_IRQHandler,      			/* DMA1 Stream3 global interrupt                                      */
-  (uint32_t)&DMA1_Stream4_IRQHandler,      			/* DMA1 Stream4 global interrupt                                      */
-  (uint32_t)&DMA1_Stream5_IRQHandler,      			/* DMA1 Stream5 global interrupt                                      */
-  (uint32_t)&DMA1_Stream6_IRQHandler,      			/* DMA1 Stream6 global interrupt                                      */
-  (uint32_t)&ADC_IRQHandler,               			/* ADC1 global interrupt                                              */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  (uint32_t)&EXTI9_5_IRQHandler,           			/* EXTI Line[9:5] interrupts                                          */
-  (uint32_t)&TIM1_BRK_TIM9_IRQHandler,     			/* TIM1 Break interrupt and TIM9 global interrupt                     */
-  (uint32_t)&TIM1_UP_TIM10_IRQHandler,     			/* TIM1 Update interrupt and TIM10 global interrupt                   */
-  (uint32_t)&TIM1_TRG_COM_TIM11_IRQHandler,			/* TIM1 Trigger and Commutation interrupts and TIM11 global interrupt */
-  (uint32_t)&TIM1_CC_IRQHandler,           			/* TIM1 Capture Compare interrupt                                     */
-  (uint32_t)&TIM2_IRQHandler,              			/* TIM2 global interrupt                                              */
-  (uint32_t)&TIM3_IRQHandler,              			/* TIM3 global interrupt                                              */
-  (uint32_t)&TIM4_IRQHandler,              			/* TIM4 global interrupt                                              */
-  (uint32_t)&I2C1_EV_IRQHandler,           			/* I2C1 event interrupt                                               */
-  (uint32_t)&I2C1_ER_IRQHandler,           			/* I2C1 error interrupt                                               */
-  (uint32_t)&I2C2_EV_IRQHandler,           			/* I2C2 event interrupt                                               */
-  (uint32_t)&I2C2_ER_IRQHandler,           			/* I2C2 error interrupt                                               */
-  (uint32_t)&SPI1_IRQHandler,              			/* SPI1 global interrupt                                              */
-  (uint32_t)&SPI2_IRQHandler,              			/* SPI2 global interrupt                                              */
-  (uint32_t)&USART1_IRQHandler,            			/* USART1 global interrupt                                            */
-  (uint32_t)&USART2_IRQHandler,            			/* USART2 global interrupt                                            */
-  0,                            			/* Reserved                                                           */
-  (uint32_t)&EXTI15_10_IRQHandler,         			/* EXTI Line[15:10] interrupts                                        */
-  (uint32_t)&RTC_Alarm_IRQHandler,         			/* RTC Alarms (A and B) through EXTI line interrupt                   */
-  (uint32_t)&OTG_FS_WKUP_IRQHandler,       			/* USB On-The-Go FS Wakeup through EXTI line interrupt                */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  (uint32_t)&DMA1_Stream7_IRQHandler,      			/* DMA1 Stream7 global interrupt                                      */
-  0,                            			/* Reserved                                                           */
-  (uint32_t)&SDIO_IRQHandler,              			/* SDIO global interrupt                                              */
-  (uint32_t)&TIM5_IRQHandler,              			/* TIM5 global interrupt                                              */
-  (uint32_t)&SPI3_IRQHandler,              			/* SPI3 global interrupt                                              */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  (uint32_t)&DMA2_Stream0_IRQHandler,      			/* DMA2 Stream0 global interrupt                                      */
-  (uint32_t)&DMA2_Stream1_IRQHandler,      			/* DMA2 Stream1 global interrupt                                      */
-  (uint32_t)&DMA2_Stream2_IRQHandler,      			/* DMA2 Stream2 global interrupt                                      */
-  (uint32_t)&DMA2_Stream3_IRQHandler,      			/* DMA2 Stream3 global interrupt                                      */
-  (uint32_t)&DMA2_Stream4_IRQHandler,      			/* DMA2 Stream4 global interrupt                                      */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  (uint32_t)&OTG_FS_IRQHandler,            			/* USB On The Go FS global interrupt                                  */
-  (uint32_t)&DMA2_Stream5_IRQHandler,      			/* DMA2 Stream5 global interrupt                                      */
-  (uint32_t)&DMA2_Stream6_IRQHandler,      			/* DMA2 Stream6 global interrupt                                      */
-  (uint32_t)&DMA2_Stream7_IRQHandler,      			/* DMA2 Stream7 global interrupt                                      */
-  (uint32_t)&USART6_IRQHandler,            			/* USART6 global interrupt                                            */
-  (uint32_t)&I2C3_EV_IRQHandler,           			/* I2C3 event interrupt                                               */
-  (uint32_t)&I2C3_ER_IRQHandler,           			/* I2C3 error interrupt                                               */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  (uint32_t)&FPU_IRQHandler,               			/* FPU global interrupt                                               */
-  0,                            			/* Reserved                                                           */
-  0,                            			/* Reserved                                                           */
-  (uint32_t)&SPI4_IRQHandler,              			/* SPI 4 global interrupt                                             */  
+    /* Cortex-M core exceptions */
+    (uint32_t)&Reset_Handler,          /* Reset */
+    (uint32_t)&NMI_Handler,            /* NMI */
+    (uint32_t)&HardFault_Handler,      /* HardFault */
+    (uint32_t)&MemManage_Handler,      /* MemManage */
+    (uint32_t)&BusFault_Handler,       /* BusFault */
+    (uint32_t)&UsageFault_Handler,     /* UsageFault */
+    0, 0, 0, 0,                        /* reserved */
+    (uint32_t)&SVC_Handler,            /* SVCall */
+    (uint32_t)&DebugMon_Handler,       /* DebugMon */
+    0,                                 /* reserved */
+    (uint32_t)&PendSV_Handler,         /* PendSV */
+    (uint32_t)&SysTick_Handler,        /* SysTick */
 
-
-
+    /* External interrupts */
+    (uint32_t)&WWDG_IRQHandler,              /* Window Watchdog */
+    (uint32_t)&PVD_IRQHandler,               /* PVD through EXTI Line detect */
+    (uint32_t)&TAMP_STAMP_IRQHandler,        /* Tamper and TimeStamp */
+    (uint32_t)&RTC_WKUP_IRQHandler,          /* RTC Wakeup */
+    (uint32_t)&FLASH_IRQHandler,             /* FLASH */
+    (uint32_t)&RCC_IRQHandler,               /* RCC */
+    (uint32_t)&EXTI0_IRQHandler,             /* EXTI Line0 */
+    (uint32_t)&EXTI1_IRQHandler,             /* EXTI Line1 */
+    (uint32_t)&EXTI2_IRQHandler,             /* EXTI Line2 */
+    (uint32_t)&EXTI3_IRQHandler,             /* EXTI Line3 */
+    (uint32_t)&EXTI4_IRQHandler,             /* EXTI Line4 */
+    (uint32_t)&DMA1_Stream0_IRQHandler,      /* DMA1 Stream0 */
+    (uint32_t)&DMA1_Stream1_IRQHandler,      /* DMA1 Stream1 */
+    (uint32_t)&DMA1_Stream2_IRQHandler,      /* DMA1 Stream2 */
+    (uint32_t)&DMA1_Stream3_IRQHandler,      /* DMA1 Stream3 */
+    (uint32_t)&DMA1_Stream4_IRQHandler,      /* DMA1 Stream4 */
+    (uint32_t)&DMA1_Stream5_IRQHandler,      /* DMA1 Stream5 */
+    (uint32_t)&DMA1_Stream6_IRQHandler,      /* DMA1 Stream6 */
+    (uint32_t)&ADC_IRQHandler,               /* ADC1 */
+    /* no DAC on F411: slot left reserved */
+    0,                                       /* Reserved */
+    0,                                       /* Reserved */
+    0,                                       /* Reserved */
+    0,                                       /* Reserved */
+    (uint32_t)&EXTI9_5_IRQHandler,           /* EXTI Lines 9:5 */
+    (uint32_t)&TIM1_BRK_TIM9_IRQHandler,     /* TIM1 Break & TIM9 */
+    (uint32_t)&TIM1_UP_TIM10_IRQHandler,     /* TIM1 Update & TIM10 */
+    (uint32_t)&TIM1_TRG_COM_TIM11_IRQHandler,/* TIM1 Trigger & Commutation & TIM11 */
+    (uint32_t)&TIM1_CC_IRQHandler,           /* TIM1 Capture Compare */
+    (uint32_t)&TIM2_IRQHandler,              /* TIM2 */
+    (uint32_t)&TIM3_IRQHandler,              /* TIM3 */
+    (uint32_t)&TIM4_IRQHandler,              /* TIM4 */
+    (uint32_t)&I2C1_EV_IRQHandler,           /* I2C1 Event */
+    (uint32_t)&I2C1_ER_IRQHandler,           /* I2C1 Error */
+    (uint32_t)&I2C2_EV_IRQHandler,           /* I2C2 Event */
+    (uint32_t)&I2C2_ER_IRQHandler,           /* I2C2 Error */
+    (uint32_t)&SPI1_IRQHandler,              /* SPI1 */
+    (uint32_t)&SPI2_IRQHandler,              /* SPI2 */
+    (uint32_t)&USART1_IRQHandler,            /* USART1 */
+    (uint32_t)&USART2_IRQHandler,            /* USART2 */
+    0,                                       /* Reserved */
+    (uint32_t)&EXTI15_10_IRQHandler,         /* EXTI Lines 15:10 */
+    (uint32_t)&RTC_Alarm_IRQHandler,         /* RTC Alarm (A and B) */
+    (uint32_t)&OTG_FS_WKUP_IRQHandler,       /* USB OTG FS Wakeup */
+    
+    /* F411 has no TIM8/9... leave these reserved or rename if not used */
+    (uint32_t)&TIM8_BRK_TIM12_IRQHandler,    /* TIM8 Break & TIM12 */
+    (uint32_t)&TIM8_UP_TIM13_IRQHandler,     /* TIM8 Update & TIM13 */
+    (uint32_t)&TIM8_TRG_COM_TIM14_IRQHandler,/* TIM8 Trigger & Commutation & TIM14 */
+    (uint32_t)&TIM8_CC_IRQHandler,           /* TIM8 Capture Compare */
+    
+    (uint32_t)&DMA1_Stream7_IRQHandler,      /* DMA1 Stream7 */
+    (uint32_t)&SDIO_IRQHandler,              /* SDIO (if present) */
+    (uint32_t)&TIM5_IRQHandler,              /* TIM5 */
+    (uint32_t)&SPI3_IRQHandler,              /* SPI3 */
+    (uint32_t)&UART4_IRQHandler,             /* UART4 */
+    (uint32_t)&UART5_IRQHandler,             /* UART5 */
+    (uint32_t)&TIM6_DAC_IRQHandler,          /* TIM6 & DAC */
+    (uint32_t)&TIM7_IRQHandler,              /* TIM7 */
+    (uint32_t)&DMA2_Stream0_IRQHandler,      /* DMA2 Stream0 */
+    (uint32_t)&DMA2_Stream1_IRQHandler,      /* DMA2 Stream1 */
+    (uint32_t)&DMA2_Stream2_IRQHandler,      /* DMA2 Stream2 */
+    (uint32_t)&DMA2_Stream3_IRQHandler,      /* DMA2 Stream3 */
+    (uint32_t)&DMA2_Stream4_IRQHandler,      /* DMA2 Stream4 */
+    (uint32_t)&CAN2_TX_IRQHandler,           /* CAN2 TX */
+    (uint32_t)&CAN2_RX0_IRQHandler,          /* CAN2 RX0 */
+    (uint32_t)&CAN2_RX1_IRQHandler,          /* CAN2 RX1 */
+    (uint32_t)&CAN2_SCE_IRQHandler,          /* CAN2 SCE */
+    (uint32_t)&OTG_FS_IRQHandler,            /* USB OTG FS */
+    (uint32_t)&DMA2_Stream5_IRQHandler,      /* DMA2 Stream5 */
+    (uint32_t)&DMA2_Stream6_IRQHandler,      /* DMA2 Stream6 */
+    (uint32_t)&DMA2_Stream7_IRQHandler,      /* DMA2 Stream7 */
+    (uint32_t)&USART6_IRQHandler,            /* USART6 */
+    (uint32_t)&I2C3_EV_IRQHandler,           /* I2C3 Event */
+    (uint32_t)&I2C3_ER_IRQHandler,           /* I2C3 Error */
+    (uint32_t)&OTG_HS_EP1_OUT_IRQHandler,    /* OTG HS EP1 OUT */
+    (uint32_t)&OTG_HS_EP1_IN_IRQHandler,     /* OTG HS EP1 IN */
+    (uint32_t)&OTG_HS_WKUP_IRQHandler,       /* OTG HS Wakeup */
+    (uint32_t)&OTG_HS_IRQHandler,            /* OTG HS */
+    (uint32_t)&DCMI_IRQHandler,              /* DCMI */
+    (uint32_t)&FPU_IRQHandler,               /* FPU global */
+    (uint32_t)&SPI4_IRQHandler,              /* SPI4 */
+    (uint32_t)&SAI1_IRQHandler,              /* SAI1 */
+    (uint32_t)&SAI2_IRQHandler,              /* SAI2 */
+    (uint32_t)&QuadSPI_IRQHandler,           /* QUADSPI */
+    (uint32_t)&HDMI_CEC_IRQHandler,          /* HDMI CEC */
+    (uint32_t)&SPDIF_Rx_IRQHandler,          /* SPDIF Rx */
+    (uint32_t)&FMPI2C1_IRQHandler,           /* FMPI2C1 Event */
+    (uint32_t)&FMPI2C1_error_IRQHandler,     /* FMPI2C1 Error */
 };
 
 /* Default handler that enters an infinite loop */
